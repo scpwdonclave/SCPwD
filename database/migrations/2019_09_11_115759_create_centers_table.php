27b015d65b18db171d3c8d481f1e3b802331a22d
saha2052 <s.saha2052@gmail.com>
2019-09-13 12:25:28 +0530

    Ongoing Admin TC Section

--- database/migrations/2019_09_11_115759_create_centers_table.php
+++ database/migrations/2019_09_11_115759_create_centers_table.php
@@ -9,15 +9,46 @@ class CreateCentersTable extends Migration
      * Run the migrations.
      *
      * @return void
-     */ 
+     */  
     public function up()
     {
         Schema::create('centers', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('tc_id')->unique();
+            $table->string('trp_id');
+            $table->string('tc_id')->unique()->nullable();
             $table->string('spoc_name');
             $table->string('email')->unique();
-            $table->string('password');
+            $table->string('spoc_mobile')->unique();
+            $table->string('password')->nullable();
+
+            $table->string('center_name');
+            $table->text('center_address');
+            $table->text('landmark');
+           
+
+            $table->string('state_district');
+            $table->string('city');
+            $table->string('block');
+            $table->string('parliament');
+            $table->string('pin');
+            $table->string('addr_proof');
+            $table->string('addr_doc');
+
+            $table->string('job_role');
+
+            $table->string('center_front_view')->nullable();
+            $table->string('center_back_view')->nullable();
+            $table->string('center_right_view')->nullable();
+            $table->string('center_left_view')->nullable();
+            $table->string('class_room_1')->nullable();
+            $table->string('lab_room_1')->nullable();
+            $table->string('equipment_1')->nullable();
+            $table->string('washroom_1')->nullable();
+            $table->string('biomertic')->nullable();
+            $table->string('drinking')->nullable();
+            $table->string('saftey')->nullable();
+            
+            
             $table->boolean('status')->default(1);
             $table->rememberToken();
             $table->timestamps();
