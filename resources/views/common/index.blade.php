9fc03a629400acc4adff776e1af5100efeee8b5d
ItsSVK <35792535+ItsSVK@users.noreply.github.com>
2019-12-14 13:06:09 +0530

    Common Login Page Re-modified, Optimised

--- resources/views/common/index.blade.php
+++ resources/views/common/index.blade.php
@@ -1,63 +1,74 @@
 @extends('custom-logins.app-index')
 @section('content')
-  <div class="testimonials">
-		<div class="card" onclick="location.href='{{route('partner.login')}}'">
-			<div class="layer"></div>
-			<div class="content">
-				<p>A Portal For Training Partners. Manage your Training Centers, Trainers, Candidates, Batches</p>
-				<div class="image">
-					<img src="https://picsum.photos/200">
+<div class="limiter">
+	<div class="container-login100">
+		<div class="wrap-login100">
+			<div class="login100-pic-mobile">
+				<img src="{{asset('assets/images/scpwd-trans.png')}}" alt="IMG">
+			</div>
+			<div class="login100-pic">
+				<div class="logo">
+					<img src="{{asset('assets/images/scpwd-trans.png')}}" alt="IMG">
 				</div>
-				<div class="details">
-					<h2>
-						Training Partners<br><span>Login to SCPwD</span>
-					</h2>
+				<div class="js-tilt" data-tilt>
+					<img src="{{asset('assets/images/geek.png')}}" alt="IMG">
 				</div>
 			</div>
-		</div>
 
-		<div class="card" onclick="location.href='{{route('center.login')}}'">
-			<div class="layer"></div>
-			<div class="content">
-				<p>A Portal For Training Centers. Login to Manage Candidates, view Batches and Stats</p>
-				<div class="image">
-					<img src="https://picsum.photos/200">
+			<div class="login100-form validate-form">
+				<span class="login100-form-title">
+					Partner Management System
+				</span>
+
+					
+				<div class="container-login100-form-btn">
+					<button class="login100-form-btn" onclick="location.href='{{route('partner.login')}}'">
+						Training Partner
+					</button>
 				</div>
-				<div class="details">
-					<h2>
-						Training Centers<br><span>Login to SCPwD</span>
-					</h2>
+				<div class="container-login100-form-btn" onclick="location.href='{{route('center.login')}}'">
+					<button class="login100-form-btn">
+						Traiing Center
+					</button>
 				</div>
-			</div>
-		</div>
-		
-		<div class="card" onclick="location.href='{{route('agency.login')}}'">
-			<div class="layer"></div>
-			<div class="content">
-				<p>A Portal For Assessment Agencies. Login to Manage Assessors, Batchs view Stats</p>
-				<div class="image">
-					<img src="https://picsum.photos/200">
+				<div class="container-login100-form-btn" onclick="location.href='{{route('agency.login')}}'">
+					<button class="login100-form-btn">
+						Assessment Agency
+					</button>
 				</div>
-				<div class="details">
-					<h2>
-						Assessment Agencies<br><span>Login to SCPwD</span>
-					</h2>
+				<div class="container-login100-form-btn" onclick="location.href='{{route('assessor.login')}}'">
+					<button class="login100-form-btn">
+						Assessor
+					</button>
 				</div>
-			</div>
-		</div>
-		<div class="card" onclick="location.href='{{route('assessor.login')}}'">
-			<div class="layer"></div>
-			<div class="content">
-				<p>A Portal For Assessors. Login To Manage Batchs and assessments view Stats</p>
-				<div class="image">
-					<img src="https://picsum.photos/200">
+
+				<div class="text-center p-t-12">
+					<a href="#TermsModal" class="custom-btn txt2" data-toggle="modal" data-target="#TermsModal">Terms & Conditions</a>
 				</div>
-				<div class="details">
-					<h2>
-						Assessors<br><span>Login to SCPwD</span>
-					</h2>
+
+				<div class="text-center p-t-25">
+					<button class="custom-btn txt2" onclick="location.href='{{route('partner.register')}}'">
+						Training Partner Registration
+						<i class="zmdi zmdi-long-arrow-right m-l-5" aria-hidden="true"></i>
+					</button>
 				</div>
+
 			</div>
 		</div>
 	</div>
+</div>
 @endsection
+@section('modal')
+    <div class="modal fade" id="TermsModal" tabindex="-1" role="dialog">
+        <div class="modal-dialog modal-lg" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h4 class="title" id="TermsModalLabel">Company Terms & Conditions</h4>
+                </div>
+                <div class="modal-body">
+                    {{Config::get('constants.declaration')}}
+                </div>
+            </div>
+        </div>
+    </div>
+@stop
--- resources/views/custom-logins/app-index.blade.php
+++ resources/views/custom-logins/app-index.blade.php
@@ -4,10 +4,30 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="icon" href="{{ asset('favicon.ico') }}" type="image/x-icon"> <!-- Favicon-->
     <title>Welcome To SCPwD | Skill Council for Persons with Disability</title>
-    <link rel="stylesheet" href="{{ asset('assets/css/index-login.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/plugins/bootstrap/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/index-util.css') }}">
+	<link rel="stylesheet" type="text/css" href="{{ asset('assets/css/index-main.css') }}">
+    <link rel="stylesheet" href="{{asset('assets/css/main.css')}}">
 </head>
 <body>
     @yield('content')
+    
+    @if (trim($__env->yieldContent('modal')))
+        @yield('modal')
+    @endif
+
+    <script src="{{asset('assets/plugins/jquery/jquery-v3.2.1.min.js')}}"></script>
+    <script src="{{ asset('assets/bundles/libscripts.bundle.js') }}"></script>    
+    <script src="{{asset('assets/js/tilt.jquery.min.js')}}"></script>
+    @if (trim($__env->yieldContent('page-script')))
+        @yield('page-script')
+    @endif
+    <script >
+		$('.js-tilt').tilt({
+			scale: 1.1
+		})
+	</script>
 </body>
 </html>
