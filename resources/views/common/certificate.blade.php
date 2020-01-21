36eb9d6b4c0659f16afa1f37ee0ac9ce25faebf3
ItsSVK <connectshouvik@gmail.com>
2020-01-21 14:12:05 +0530

    certificate fix

--- resources/views/common/certificate.blade.php
+++ resources/views/common/certificate.blade.php
@@ -8,7 +8,7 @@ span.cls_002{font-family:Times,serif;font-size:16px;color:rgb(0,0,0);font-weight
 div.cls_002{font-family:Times,serif;font-size:16px;color:rgb(0,0,0);font-weight:normal;font-style:normal;text-decoration: none}
 span.cls_003{font-family:Times,serif;font-size:16px;color:rgb(0,0,0);font-weight:bold;font-style:normal;text-decoration: none}
 div.cls_003{font-family:Times,serif;font-size:16px;color:rgb(0,0,0);font-weight:bold;font-style:normal;text-decoration: none}
-span.cls_004{font-family:Times,serif;font-size:25px;color:rgb(0,0,0);font-weight:bold;font-style:normal;text-decoration: none}
+span.cls_004{font-family:Times,serif;font-size:24px;color:rgb(0,0,0);font-weight:bold;font-style:normal;text-decoration: none}
 div.cls_004{font-family:Times,serif;font-size:25px;color:rgb(0,0,0);font-weight:bold;font-style:normal;text-decoration: none}
 span.cls_005{font-family:Times,serif;font-size:60px;color:rgb(255,153,51);font-weight:bold;font-style:normal;text-decoration: none}
 div.cls_005{font-family:Times,serif;font-size:60px;color:rgb(255,153,51);font-weight:bold;font-style:normal;text-decoration: none}
@@ -40,7 +40,7 @@ div.cls_009{font-family:Times,serif;font-size:11.1px;color:rgb(0,0,0);font-weigh
 
 <div style="position:absolute;left:360px;top:330px" class="cls_004"><span class="cls_004">Department of Empowerment of Persons with Disabilities (Divyangjan)</span></div>
 <div style="position:absolute;left:510px;top:360px" class="cls_004"><span class="cls_004">Ministry of Social Justice and Empowerment</span></div>
-<div style="position:absolute;left:650px;top:390px" class="cls_005"><span class="cls_005">Certificate</span></div>
+<div style="position:absolute;left:630px;top:390px" class="cls_005"><span class="cls_005">Certificate</span></div>
 
 <div style="height:250px;width:1000px;position:absolute;left:290px;top:470px;" class="cls_006">
     <span class="cls_006">This is to certify that </span>
