5b00a7b0a44d63c30f4f182042565a8ea9910d95
ItsSVK <35792535+ItsSVK@users.noreply.github.com>
2020-04-25 00:17:34 +0530

    Old Candidate Seeds Created

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -23,5 +23,6 @@ class DatabaseSeeder extends Seeder
         $this->call(JobQualificationsTableSeeder::class);
         $this->call(SchemesTableSeeder::class);
         $this->call(DepartmentsTableSeeder::class);
+        $this->call(OldCandidatesTableSeeder::class);
     }
 }
