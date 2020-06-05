f9db45d7c8e5a94dba50c3ffc8f11536d878ec9b
ItsSVK <35792535+ItsSVK@users.noreply.github.com>
2020-06-05 23:04:19 +0530

    partner certificate issue fix

--- resources/views/common/certificate.blade.php
+++ resources/views/common/certificate.blade.php
@@ -1,5 +1,5 @@
 <html>
-<title>{{isset($assessment)?$assessment->batch->batch_id:$batch->batch_id}}</title>
+<title>{{isset($assessment)?$batchdata->batch_id:$batch->batch_id}}</title>
 <head><meta http-equiv=Content-Type content="text/html; charset=UTF-8">        
 <style type="text/css">
  
@@ -27,6 +27,7 @@ div.cls_009{font-family:Times,serif;font-size:11.1px;color:rgb(0,0,0);font-weigh
 
     @php
         $data = isset($assessment)?$assessment->candidateMarks:$batch->candidatesmap;
+        $batchdata = isset($assessment)?$assessment->batch:$batch;
     @endphp
 
     @foreach ($data as $item)
@@ -38,60 +39,60 @@ div.cls_009{font-family:Times,serif;font-size:11.1px;color:rgb(0,0,0);font-weigh
         @if($flag)
 
             <div style="height:1000px;width:1500px;position:relative">
-                @if ($assessment->batch->scheme->scheme === 'DEPwD-SIPDA 19-20')
+                @if ($batchdata->scheme->scheme === 'DEPwD-SIPDA 19-20')
                 <img src="{{asset('assets/images/certificate/SIPDA.jpg')}}" style="height:1000px;width:1500px;">
                     
-                @elseif($assessment->batch->scheme->scheme === 'NHFDC_CSR' || $assessment->batch->scheme->scheme === 'TOT')
+                @elseif($batchdata->scheme->scheme === 'NHFDC_CSR' || $batchdata->scheme->scheme === 'TOT')
                 <img src="{{asset('assets/images/certificate/NHFDC-CSR.jpg')}}" style="height:1000px;width:1500px;">
-                @elseif($assessment->batch->scheme->scheme === 'AYJNISHD-SIPDA')
+                @elseif($batchdata->scheme->scheme === 'AYJNISHD-SIPDA')
                 <img src="{{asset('assets/images/certificate/AYJNISHD.jpg')}}" style="height:1000px;width:1500px;">
-                @elseif($assessment->batch->scheme->scheme === 'NHFDC-SIPDA 18-19')
+                @elseif($batchdata->scheme->scheme === 'NHFDC-SIPDA 18-19')
                 <img src="{{asset('assets/images/certificate/NHFDC.jpg')}}" style="height:1000px;width:1500px;">
-                @elseif($assessment->batch->scheme->scheme === 'NIEPMD-SIPDA')
+                @elseif($batchdata->scheme->scheme === 'NIEPMD-SIPDA')
                 <img src="{{asset('assets/images/certificate/NIEPMD.jpg')}}" style="height:1000px;width:1500px;">
-                @elseif($assessment->batch->scheme->scheme === 'NILD-SIPDA')
+                @elseif($batchdata->scheme->scheme === 'NILD-SIPDA')
                 <img src="{{asset('assets/images/certificate/NILD.jpg')}}" style="height:1000px;width:1500px;">
-                @elseif($assessment->batch->scheme->scheme === 'PDUNIPPD-SIPDA')
+                @elseif($batchdata->scheme->scheme === 'PDUNIPPD-SIPDA')
                 <img src="{{asset('assets/images/certificate/PDUNIPPD.jpg')}}" style="height:1000px;width:1500px;">
                   
                 @endif
                 <div style="position:absolute;left:138px;top:125px;height:30px"  class="cls_002"><span class="cls_002">Certificate No: </span><span class="cls_003">{{isset($assessment)?$item->centerCandidate->certi_no:$item->centercandidate->certi_no}}</span></div>
                 {{-- <div style="position:absolute;left:138px;top:160px;height:100px;">     --}}
-                    {{-- <img src="{{asset('storage/'.(isset($assessment)?$assessment->batch->scheme->logo:$batch->scheme->logo))}}" style="width:180px;"> --}}
+                    {{-- <img src="{{asset('storage/'.(isset($assessment)?$batchdata->scheme->logo:$batch->scheme->logo))}}" style="width:180px;"> --}}
                 {{-- </div> --}}
 
-                @if ($assessment->batch->scheme->scheme === 'DEPwD-SIPDA 19-20')
+                @if ($batchdata->scheme->scheme === 'DEPwD-SIPDA 19-20')
                 <div style="position:absolute;left:385px;top:330px" class="cls_004"><span class="cls_004">Department of Empowerment of Persons with Disabilities (Divyangjan)</span></div>
                 <div style="position:absolute;left:510px;top:360px" class="cls_004"><span class="cls_004">Ministry of Social Justice and Empowerment</span></div>
                 <div style="position:absolute;left:625px;top:390px" class="cls_005"><span class="cls_005">Certificate</span></div>
                     
-                @elseif($assessment->batch->scheme->scheme === 'NHFDC_CSR' || $assessment->batch->scheme->scheme === 'TOT')
+                @elseif($batchdata->scheme->scheme === 'NHFDC_CSR' || $batchdata->scheme->scheme === 'TOT')
                 <div style="position:absolute;left:385px;top:330px;width:800px;text-align:center;"><span style="font-size:29px;font-weight:bold">National Handicapped Finance & Development Corporation</span><br>
                 <span style="font-size:22px;">(A PSU under the Ministry of Social Justice & Empowerment Department of Empowerment of Persons with Disabilities (Divyangjan), Government of India)</span></div><br>
                 <div style="position:absolute;left:625px;top:410px" class="cls_005"><span class="cls_005">Certificate</span></div>   
-                @elseif($assessment->batch->scheme->scheme === 'AYJNISHD-SIPDA')
+                @elseif($batchdata->scheme->scheme === 'AYJNISHD-SIPDA')
                 <div style="position:absolute;left:330px;top:330px;width:850px;text-align:center;">
                     <span style="font-size:24px;font-weight:bold">Ali Yavar Jung</span><br>
                     <span style="font-size:24px;font-weight:bold">National Institute of Speech and Hearing Disabilities (Divyangjan) </span><br>
                     <span style="font-size:24px;font-weight:bold">Department of Empowerment of Persons with Disabilities (Divyangjan), Ministry of Social Justice and Empowerment  </span></div><br>
                 
                 <div style="position:absolute;left:625px;top:420px;padding:13px" class="cls_005"><span class="cls_005">Certificate</span></div>   
-                @elseif($assessment->batch->scheme->scheme === 'NHFDC-SIPDA 18-19')
+                @elseif($batchdata->scheme->scheme === 'NHFDC-SIPDA 18-19')
                 <div style="position:absolute;left:385px;top:330px;width:800px;text-align:center;">
                 <span style="font-size:24px;font-weight:bold">National Handicapped Finance & Development Corporation</span><br>
                 <span style="font-size:24px;font-weight:bold">Department of Empowerment of Persons with Disabilities (Divyangjan), Ministry of Social Justice and Empowerment </span></div><br>
                 <div style="position:absolute;left:625px;top:410px" class="cls_005"><span class="cls_005">Certificate</span></div> 
-                @elseif($assessment->batch->scheme->scheme === 'NIEPMD-SIPDA')
+                @elseif($batchdata->scheme->scheme === 'NIEPMD-SIPDA')
                 <div style="position:absolute;left:300px;top:330px;width:900px;text-align:center;">
                 <span style="font-size:24px;font-weight:bold">National Institute for Empowerment of Persons with Multiple Disabilities (Divyangjan),</span><br>
                 <span style="font-size:24px;font-weight:bold">Department of Empowerment of Persons with Disabilities (Divyangjan),<br>Ministry of Social Justice & Empowerment, Govt. of India  </span></div><br>
                 <div style="position:absolute;left:625px;top:410px" class="cls_005"><span class="cls_005">Certificate</span></div> 
-                @elseif($assessment->batch->scheme->scheme === 'NILD-SIPDA')
+                @elseif($batchdata->scheme->scheme === 'NILD-SIPDA')
                 <div style="position:absolute;left:300px;top:330px;width:900px;text-align:center;">
                 <span style="font-size:25px;font-weight:bold">National Institute for Locomotor Disabilities (Divyangjan) </span><br>
                 <span style="font-size:25px;font-weight:bold">Department of Empowerment of Persons with Disabilities (Divyangjan),<br>Ministry of Social Justice and Empowerment </span></div><br>
                 <div style="position:absolute;left:625px;top:415px" class="cls_005"><span class="cls_005">Certificate</span></div> 
-                @elseif($assessment->batch->scheme->scheme === 'PDUNIPPD-SIPDA')
+                @elseif($batchdata->scheme->scheme === 'PDUNIPPD-SIPDA')
                 <div style="position:absolute;left:260px;top:330px;width:1000px;text-align:center;">
                 <span style="font-size:25px;font-weight:bold">Pt. Deendayal Upadhyaya National Institute for Persons with Physical Disability (Divyangjan)</span>
                 <span style="font-size:25px;font-weight:bold">Department of Empowerment of Persons with Disabilities,<br>Ministry of Social Justice & Empowerment, Govt. of India</span>
@@ -99,7 +100,7 @@ div.cls_009{font-family:Times,serif;font-size:11.1px;color:rgb(0,0,0);font-weigh
                 </div><br>
                 <div style="position:absolute;left:625px;top:415px" class="cls_005"><span class="cls_005">Certificate</span></div> 
                 @endif
-                @if ($assessment->batch->scheme->scheme === 'AYJNISHD-SIPDA')
+                @if ($batchdata->scheme->scheme === 'AYJNISHD-SIPDA')
                 <div style="height:250px;width:1000px;position:absolute;left:290px;top:510px;text-align:center" class="cls_006">
                     
                 @else
@@ -161,10 +162,10 @@ div.cls_009{font-family:Times,serif;font-size:11.1px;color:rgb(0,0,0);font-weigh
                         <span class="cls_007">*****{{substr((isset($assessment)?$item->centerCandidate->candidate->doc_no:$item->centercandidate->candidate->doc_no),-5)}}</span>  
                     @endif
                     <span class="cls_006">has successfully completed the course of</span>
-                    <span class="cls_007"> {{isset($assessment)?$assessment->batch->jobrole->job_role:$batch->jobrole->job_role}}  (QP No.  {{isset($assessment)?$assessment->batch->jobrole->qp_code:$batch->jobrole->qp_code}}) </span>
-                    <span class="cls_006">conforming to National Skill Qualifications Framework - Level </span><span class="cls_007">{{isset($assessment)?$assessment->batch->jobrole->nsqf_level:$batch->jobrole->nsqf_level}}</span>
+                    <span class="cls_007"> {{isset($assessment)?$batchdata->jobrole->job_role:$batch->jobrole->job_role}}  (QP No.  {{isset($assessment)?$batchdata->jobrole->qp_code:$batch->jobrole->qp_code}}) </span>
+                    <span class="cls_006">conforming to National Skill Qualifications Framework - Level </span><span class="cls_007">{{isset($assessment)?$batchdata->jobrole->nsqf_level:$batch->jobrole->nsqf_level}}</span>
                     <span class="cls_006">through</span>
-                    <span class="cls_007"> {{isset($assessment)?$assessment->batch->center->center_name:$batch->center->center_name}}, {{isset($assessment)?$assessment->batch->center->city:$batch->center->city}} </span>
+                    <span class="cls_007"> {{isset($assessment)?$batchdata->center->center_name:$batch->center->center_name}}, {{isset($assessment)?$batchdata->center->city:$batch->center->city}} </span>
                     <span class="cls_006">in the month of</span>
                     @php
                         $assessment_certi_issued_on = isset($assessment)?$item->centerCandidate->assessment_certi_issued_on:$item->centercandidate->assessment_certi_issued_on;
