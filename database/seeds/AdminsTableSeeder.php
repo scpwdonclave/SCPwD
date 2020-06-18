0035b77ff265718a5b798659667918bb9d1fb13b
ItsSVK <35792535+ItsSVK@users.noreply.github.com>
2020-06-18 14:47:31 +0530

    Seeds Updated

--- database/seeds/AdminsTableSeeder.php
+++ database/seeds/AdminsTableSeeder.php
@@ -20,9 +20,9 @@ class AdminsTableSeeder extends Seeder
             0 => 
             array (
                 'id' => 1,
-                'name' => 'Super Admin',
-                'email' => 'superadmin@gmail.com',
-                'password' => '$2y$10$0qBeLAhWQ.WLYN98MPIV9eiqtEBUtzZ6Rm.wa9uAGLc9YW0ARTHEK',
+                'name' => 'Anup Srivastava',
+                'email' => 'anup.srivastava@scpwd.in',
+                'password' => '$2y$10$vakQaFxOJnsciritYVb/juaIP0yT3EQSKrte7tgg3bzJeFyKy8ao.', // Anup@123
                 'supadmin' => 1,
                 'status' => 1,
                 'remember_token' => NULL,
@@ -32,10 +32,10 @@ class AdminsTableSeeder extends Seeder
             1 => 
             array (
                 'id' => 2,
-                'name' => 'Shouvik Mohanta',
-                'email' => 'shouvik@gmail.com',
-                'password' => '$2y$10$0qBeLAhWQ.WLYN98MPIV9eiqtEBUtzZ6Rm.wa9uAGLc9YW0ARTHEK',
-                'supadmin' => 0,
+                'name' => 'Bhupesh Sharma',
+                'email' => 'bhupesh.sharma@scpwd.in',
+                'password' => '$2y$10$W2MOtYCPrtiwIJlfZqMOX.j6fBp7kDh0xNGGQzhTVXE7Ql1FWssX.', // Bhupesh@123
+                'supadmin' => 1,
                 'status' => 1,
                 'remember_token' => NULL,
                 'created_at' => Carbon::now(),
@@ -44,9 +44,21 @@ class AdminsTableSeeder extends Seeder
             2 => 
             array (
                 'id' => 3,
-                'name' => 'Sayan Saha',
-                'email' => 'sayan@gmail.com',
-                'password' => '$2y$10$0qBeLAhWQ.WLYN98MPIV9eiqtEBUtzZ6Rm.wa9uAGLc9YW0ARTHEK',
+                'name' => 'Surabhiraj Ekka',
+                'email' => 'surabhiraj.ekka@scpwd.in',
+                'password' => '$2y$10$DgGtw4XdrKkxikjPjir5w.4Y3bajhStHvTy/7ZSWwqG91O0cOPfTG', // Surabhi@123
+                'supadmin' => 1,
+                'status' => 1,
+                'remember_token' => NULL,
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ),
+            3 => 
+            array (
+                'id' => 4,
+                'name' => 'Sanju Singh',
+                'email' => 'sanju.singh@scpwd.in',
+                'password' => '$2y$10$KkAxjMj4xuKTrFTMpiu.GukAK7BCJMmj5mXAfqzHeJANv23By2Ysi', // Sanju@123
                 'supadmin' => 0,
                 'status' => 1,
                 'remember_token' => NULL,
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,13 +13,13 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(AdminsTableSeeder::class);
         $this->call(StateDistrictTableSeeder::class);
-        $this->call(SectorsTableSeeder::class);
+        // $this->call(SectorsTableSeeder::class);
         $this->call(ParliamentTableSeeder::class);
         $this->call(LanguageTableSeeder::class);
-        $this->call(ExpositoriesTableSeeder::class);
-        $this->call(ExpositoryJobRoleTableSeeder::class);
-        $this->call(JobRolesTableSeeder::class);
-        $this->call(JobQualificationsTableSeeder::class);
+        // $this->call(ExpositoriesTableSeeder::class);
+        // $this->call(ExpositoryJobRoleTableSeeder::class);
+        // $this->call(JobRolesTableSeeder::class);
+        // $this->call(JobQualificationsTableSeeder::class);
         $this->call(SchemesTableSeeder::class);
         $this->call(DepartmentsTableSeeder::class);
         $this->call(OldCandidatesTableSeeder::class);
