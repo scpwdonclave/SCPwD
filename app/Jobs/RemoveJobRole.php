a9ef8770742188c6cf66419d3ff48a1b467623c7
ItsSVK <35792535+ItsSVK@users.noreply.github.com>
2020-07-08 16:32:47 +0530

    Optimized

--- app/Jobs/RemoveJobRole.php
+++ app/Jobs/RemoveJobRole.php
@@ -42,7 +42,7 @@ class RemoveJobRole implements ShouldQueue
                 return response()->json(array('type' => 'error', 'message' => "You cannot <span style='font-weight:bold;color:red'>Remove</span> a Job Role while its associated with Training Partners"),200);
             } else {
                 $jobrole->qualifications()->delete();
-                $jobrole->expositories()->delete();
+                // $jobrole->expositories()->delete();
                 $jobrole->delete();
                 return response()->json(array('type' => 'success', 'message' => "Job Role <span style='font-weight:bold;color:blue'>$request->name</span> has been Removed Successfully"),200);
             }
