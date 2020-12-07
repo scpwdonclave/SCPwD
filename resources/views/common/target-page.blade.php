8e184e01f08b27a178764788d1f6ea08a88974d1
ItsSVK <35792535+ItsSVK@users.noreply.github.com>
2020-12-07 18:01:09 +0530

    TP Target Issue has been resolved

--- resources/views/common/target-page.blade.php
+++ resources/views/common/target-page.blade.php
@@ -20,7 +20,11 @@
                         <h4 class="margin-0">{{(Request::segment(1)==='admin')?$partner->spoc_name:$center->spoc_name}}</h4>
                         <h6 class="m-b-20" style="color:blue">{{(Request::segment(1)==='admin')?$partner->tp_id:$center->tc_id}}</h6>
                     </div>
-                    @if ( request()->segment(1)==='admin' && !auth()->guard('admin')->user()->ministry)
+                    @if ( request()->segment(1)==='admin')
+                        @if (!auth()->guard('admin')->user()->ministry)
+                            <button type="button" class="btn btn-primary btn-sm" style="float:right;" onclick="popupMenu()">Add Job Role</button>
+                        @endif
+                    @else                
                         <button type="button" class="btn btn-primary btn-sm" style="float:right;" onclick="popupMenu()">Add Job Role</button>
                     @endif
                     <div class="table-responsive">
@@ -35,7 +39,11 @@
                                     <th>Enrolled</th>
                                     <th>Achieved</th>
                                     <th>Scheme Status</th>
-                                    @if ( request()->segment(1)==='admin' && !auth()->guard('admin')->user()->ministry)
+                                    @if ( request()->segment(1)==='admin' )
+                                        @if (!auth()->guard('admin')->user()->ministry)
+                                            <th>Edit</th>
+                                        @endif
+                                    @else
                                         <th>Edit</th>
                                     @endif
                                 </tr>
@@ -63,7 +71,15 @@
                                             <td>{{$count}}</td>
                                             <td>{{$passed}}</td>
                                             <td class="text-{{($job->status)?'success':'danger'}}"><strong>{{($job->status)?'Active':'Inactive'}}</strong></td>
-                                            @if ( request()->segment(1)==='admin' && !auth()->guard('admin')->user()->ministry)
+                                            @if ( request()->segment(1)==='admin' )
+                                                @if (!auth()->guard('admin')->user()->ministry)
+                                                    @if($job->status)
+                                                        <td><button type="button" class="badge bg-green margin-0" onclick="popupMenu({{$job->id}})">Edit</button></td>
+                                                    @else
+                                                        <td><button type="button" class="badge bg-grey margin-0" onclick="return false">Edit</button></td>
+                                                    @endif
+                                                @endif
+                                            @else
                                                 @if($job->status)
                                                     <td><button type="button" class="badge bg-green margin-0" onclick="popupMenu({{$job->id}})">Edit</button></td>
                                                 @else
@@ -88,7 +104,15 @@
                                             @endphp
                                             <td>{{$passed}}</td>
                                             <td class="text-{{($job->status)?'success':'danger'}}"><strong>{{($job->status)?'Active':'Inactive'}}</strong></td>
-                                            @if ( request()->segment(1)==='admin' && !auth()->guard('admin')->user()->ministry)
+                                            @if ( request()->segment(1)==='admin' )
+                                                @if (!auth()->guard('admin')->user()->ministry)
+                                                    @if($job->status)
+                                                    <td><button type="button" class="badge bg-green margin-0" onclick="popupMenu({{$job->id}})">Edit</button></td>
+                                                @else
+                                                    <td><button type="button" class="badge bg-grey margin-0" onclick="return false">Edit</button></td>
+                                                @endif
+                                                @endif
+                                            @else
                                                 @if($job->status)
                                                     <td><button type="button" class="badge bg-green margin-0" onclick="popupMenu({{$job->id}})">Edit</button></td>
                                                 @else
